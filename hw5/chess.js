9ff206a241ea808b27a363dd1e3d98a22fb988be
HalamBalam <antonsurganov@yandex.ru>
2022-07-07 21:23:19 +0300

    hw5 corr1

--- hw5/chess.js
+++ hw5/chess.js
@@ -1,61 +1,36 @@
 class Figure {
     constructor(color) {
         this.color = color;
-        this.cell = undefined;
     }
     availableMoves() {};
-    static availableMoves(cell) {};
 }
 
 class Pawn extends Figure {
     constructor(color) {
         super(color);
-        this.currentClass = Pawn;
+        this.transformable = true;
     }
-    transform(newClass) {
-        if (newClass !== King) {
-            this.currentClass = newClass;
-        }
-    }
-    availableMoves() {
-        return this.currentClass.availableMoves(this.cell);
-    };
-    static availableMoves(cell) {};
+    availableMoves() {};
 }
 
 class Rook extends Figure {
-    availableMoves() {
-        return Rook.availableMoves(this.cell);
-    }
-    static availableMoves(cell) {};
+    availableMoves() {}
 }
 
 class Bishop extends Figure {
-    availableMoves() {
-        return Bishop.availableMoves(this.cell);
-    }
-    static availableMoves(cell) {};
+    availableMoves() {}
 }
 
 class Knight extends Figure {
-    availableMoves() {
-        return Knight.availableMoves(this.cell);
-    }
-    static availableMoves(cell) {};
+    availableMoves() {}
 }
 
 class King extends Figure {
-    availableMoves() {
-        return King.availableMoves(this.cell);
-    }
-    static availableMoves(cell) {};
+    availableMoves() {}
 }
 
 class Queen extends Figure {
-    availableMoves() {
-        return Queen.availableMoves(this.cell);
-    }
-    static availableMoves(cell) {};
+    availableMoves() {}
 }
 
 
@@ -69,18 +44,30 @@ class Board {
             }
         }
     }
-    get(cell) {
-        let row = Number(cell[1]) - 1;
-        let col = 'abcdefgh'.indexOf(cell[0].toLowerCase());
+    getFigure(cell) {
+        const row = Number(cell[1]) - 1;
+        const col = 'abcdefgh'.indexOf(cell[0].toLowerCase());
         return this.cells[row][col];
     }
-    set(cell, figure) {
-        let row = Number(cell[1]) - 1;
-        let col = 'abcdefgh'.indexOf(cell[0].toLowerCase());
+    setFigure(cell, figure) {
+        const row = Number(cell[1]) - 1;
+        const col = 'abcdefgh'.indexOf(cell[0].toLowerCase());
         this.cells[row][col] = figure;
-
-        if (figure) {
-            figure.cell = cell;
+    }
+    getCell(figure) {
+        for (let i = 0; i < this.cells.length; i++) {
+            const row = this.cells[i];
+            const ind = row.indexOf(figure);
+            if (ind >= 0) {
+                return 'abcdefgh'[ind] + String(i + 1);
+            }
+        }
+    }
+    transform(figure, newClass) {
+        if (figure.transformable && newClass !== King) {
+            const newFigure = new newClass(figure.color);
+            this.setFigure(this.getCell(figure), newFigure);
+            return newFigure;
         }
     }
 }
@@ -89,26 +76,25 @@ class Game {
     constructor(board) {
         this.board = board;
         this.currentPlayer = 'white';
-        this.winner = undefined;
+        this.status = 'active';
     }
     lostFigures = [];
     history = [];
     move(startCell, endCell) {
-        if (this.winner) {
+        if (this.status !== 'active') {
             return;
         }
-        let enemyFigure = this.board.get(endCell);
+        const enemyFigure = this.board.getFigure(endCell);
         if (enemyFigure) {
             this.lostFigures.push(enemyFigure);
-            enemyFigure.cell = undefined;
         }
-        let currentFigure = this.board.get(startCell);
-        this.board.set(startCell, undefined);
-        this.board.set(endCell, currentFigure);
+        const currentFigure = this.board.getFigure(startCell);
+        this.board.setFigure(startCell, undefined);
+        this.board.setFigure(endCell, currentFigure);
         this.history.push(startCell + ':' + endCell);
 
         if (Math.random() < 0.05) {
-            this.winner = this.currentPlayer;
+            this.status = this.currentPlayer + ' wins';
         } else {
             this.currentPlayer = this.currentPlayer === 'white' ? 'black' : 'white';
         }
@@ -124,8 +110,8 @@ const game = new Game(board);
 const whitePawn = new Pawn('white');
 const blackRook = new Rook('black');
 
-board.set('e2', whitePawn);
-board.set('h8', blackRook);
+board.setFigure('e2', whitePawn);
+board.setFigure('h8', blackRook);
 
 game.move('e2', 'e4');
 game.move('h8', 'a8');
@@ -137,9 +123,10 @@ game.move('e6', 'e7');
 game.move('f1', 'f8');
 game.move('e7', 'f8');
 
-whitePawn.transform(Queen);
+const whiteQueen = board.transform(whitePawn, Queen);
 
-if (game.winner) {
-    console.log('Игра окончена. Победитель: ' + game.winner);
+if (game.status !== 'active') {
+    console.log('Игра окончена. Результат: ' + game.status);
 }
 console.log('Ходов с начала игры: ' + game.movesFromStart());
+console.log(whiteQueen);
